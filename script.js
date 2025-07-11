4e27eadf1287d088c27dd3817fc28a3be4ecfea0
Rafafi35 <salinger.rafael@gmail.com>
2025-07-11 23:06:13 +0200

    function KarteZiehen

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let deck = [
-     2, 2, 2, 2,
-     3, 3, 3, 3,
-     4, 4, 4, 4,
-     5, 5, 5, 5,
-     6, 6, 6, 6,
-     7, 7, 7, 7,
-     8, 8, 8, 8,
-     9, 9, 9, 9,
-    10, 10, 10, 10,
-    10, 10, 10, 10,
-    10, 10, 10, 10,
-    10, 10, 10, 10,
-    11, 11, 11, 11
-]
+let fullDeck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
+let currentDeck = fullDeck
 const playerHandText = document.getElementById("playerHand")
+const log = document.getElementById("log")
+const deckSize = document.getElementById("deckSize")
 let playerHandValue = 0
 
 function karteZiehen() {
     let randomZahl = Math.floor(Math.random() * 52)
-    console.log(randomZahl)
-    playerHandValue += deck[randomZahl]
-    console.log(deck[randomZahl])
+    playerHandValue += currentDeck[randomZahl]
+    deckSize.textContent = "Decksize: " + currentDeck.length
     playerHandText.textContent = "Your Hand: " + playerHandValue
+    log.textContent = "You pulled a " + currentDeck[randomZahl]
+}
+
+function dealerZieht() {
+
 }
 
 karteZiehen()
